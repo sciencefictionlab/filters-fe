f96cf16088ad09f7107f21757126c006151ea9e3
pragati2112 <pandeypragati2112@gmail.com>
2021-10-19 00:14:28 +0530

    initial version of converting background of images to gray

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Buefy from "buefy"
+// import 'buefy/dist/buefy.css'
+require('./assets/scss/main.scss');
+
+Vue.config.productionTip = false;
+Vue.use(Buefy)
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/services/intercept.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const _axios = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+  /*adapter: cache.adapter*/
+})
+
+
+export default _axios
