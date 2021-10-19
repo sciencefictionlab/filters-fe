bebd00e2283ddcc00ca2cfbc45fe26a773e8dc3d
pragati2112 <pandeypragati2112@gmail.com>
2021-10-19 16:05:56 +0530

    ui chnages

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,52 +1,60 @@
 <template>
   <div>
-    <section class="section has-text-centered">
-      <h1 class="title is-size-3 has-text-centered mt-5">
-        This is a filter app.
-      </h1>
 
-      <b-field style="margin-top: 5rem">
-        <div class="box">
-          <b-upload v-model="file" style="cursor: pointer">
-            <section class="section">
-              <div class="content has-text-centered">
-                <p id="file">
-                  <b-icon icon="upload" size="is-large"></b-icon>
-                  {{ file.name }}
-                </p>
-                <p>Drop your files here or click to upload</p>
-              </div>
-            </section>
-          </b-upload>
-        </div>
-      </b-field>
-      <a class="button is-primary is-centered">
-        <span @click="uploadFileAndSend">click to apply filter</span>
-      </a>
-
-      <div class="box mt-5" v-if="converted_image_url">
-        <h3 class="title is-size-5">Filter applied--</h3>
-        <img
-          v-bind:src="converted_image_url"
-          alt=""
-          style="height: 20rem"
-        />
-
-        <br /><br />
-        <a
-          v-bind:href="converted_image_url"
-          target="_blank"
-          class="button is-primary is-centered"
-        >
-          <span @click="downloadImage()">Download image</span>
-        </a>
+    <section class="hero is-small is-primary has-text-centered">
+      <div class="hero-body">
+        <p class="title">
+          Photo filter app
+        </p>
       </div>
     </section>
-    <b-loading
-      :is-full-page="true"
-      v-model="isLoading"
-      :can-cancel="false"
-    ></b-loading>
+
+    <section class="section has-text-centered">
+      <div class="columns" style="margin-top: 5rem">
+        <div class="column is-4">
+          <div class="box bx-style" >
+            <b-field>
+              <b-upload v-model="file" style="cursor: pointer" @input="uploadFileAndSend">
+                <section class="section">
+                  <div class="content has-text-centered">
+                    <p id="file">
+                      <b-icon icon="upload" size="is-large"></b-icon>
+                      {{ file.name }}
+                    </p>
+                    <h5>Drop your files here or click to upload <br> <br>
+                      <span class="tag is-primary">Make background gray</span>
+                    </h5>
+                  </div>
+                </section>
+              </b-upload>
+            </b-field>
+          </div>
+        </div>
+
+        <div class="column">
+          <div class="box bx-style" v-if="converted_image_url">
+            <h3 class="title is-size-5">Preview
+
+              <a
+                v-bind:href="converted_image_url"
+                target="_blank"
+                class=" tag is-centered is-primary has-cursor-pointer">
+                <span @click="downloadImage()">Download</span>
+              </a>
+            </h3>
+            <img
+              v-bind:src="converted_image_url"
+              alt=""
+              style="height: 20rem"/>
+          </div>
+        </div>
+      </div>
+
+      <hr class="divider has-background-grey-light">
+    </section>
+
+    <b-loading :is-full-page="true"  v-model="isLoading" :can-cancel="false"></b-loading>
+
   </div>
 </template>
 
@@ -59,7 +67,7 @@ export default {
     return {
       file: {},
       isLoading: false,
-      converted_image_url: "",
+      converted_image_url: ""
     };
   },
   methods: {
@@ -70,7 +78,7 @@ export default {
       this.$data.isLoading = true;
       _axios
         .post(process.env.VUE_APP_API_URL + "/image/upload", formData, {
-          headers: { "Content-Type": "multipart/form-data" },
+          headers: { "Content-Type": "multipart/form-data" }
         })
         .then((response) => {
           this.$data.converted_image_url = response.data.url;
@@ -83,7 +91,17 @@ export default {
         });
     },
 
-    downloadImage() {},
-  },
+    downloadImage() {
+    }
+  }
 };
 </script>
+<style lang="scss" scoped>
+@import "src/assets/scss/variables";
+
+.bx-style{
+ border: 2px solid $primary;
+  background-color:#ebd1d163
+}
+
+</style>
